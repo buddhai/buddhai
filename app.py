af774b8eecb9fc823ae5b6fe7c7fc515007b4fe6
buddhai <youngchan.louis@gmail.com>
2024-09-04 17:47:09 +0900

    g

--- app.py
+++ app.py
@@ -31,13 +31,13 @@ def remove_citation_markers(text):
 
 # 단일 페르소나 설정
 ai_persona = "스님 AI"
-ai_icon = "🌸"
+ai_icon = "🪷"
 
 # 사용자 아이콘 설정
 user_icon = "🧑🏻‍💻"
 
 # Streamlit 페이지 설정
-st.set_page_config(page_title="스님 AI", page_icon="🌸", layout="wide")
+st.set_page_config(page_title="스님 AI", page_icon="🪷", layout="wide")
 
 # 커스텀 CSS 추가
 st.markdown("""
